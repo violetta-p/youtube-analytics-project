3062b3066833f1a54d20168d77dc9ed10ca5fba6
ViolettaP <violetta.pukhova@mail.ru>
2023-05-31 00:14:47 +0300

    HW_1

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,12 @@
+import json
+import os
+from googleapiclient.discovery import build
+
+# channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
+# channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+
+api_key: str = os.getenv('YOU_TUBE_API')
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
@@ -5,8 +14,9 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
