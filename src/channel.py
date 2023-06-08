868cc8350bdb551cf0518c4f7951ed5470180f59
ViolettaP <violetta.pukhova@mail.ru>
2023-06-08 17:08:34 +0300

    HW_2

--- src/channel.py
+++ src/channel.py
@@ -14,9 +14,47 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
+        self.title = None
+        self.url = None
+        self.description = None
+        self.view_count = 0
+        self.subscriber_count = 0
+        self.video_count = 0
 
-    def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(channel, indent=2, ensure_ascii=False))
+    #def print_info(self) -> None:
+    #   """Выводит в консоль информацию о канале."""
+    #   channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+    #   print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    def set_new_data(self):
+        video_response = self.get_service()
+        self.title: str = video_response['items'][0]['snippet']['title']
+        self.url: str = video_response['items'][0]['snippet']['medium']['url']
+        self.description: str = video_response['items'][0]['snippet']['description']
+        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+        self.video_count: int = video_response['items'][0]['statistics']['videoCount']
+        self.subscriber_count: int = video_response['items'][0]['statistics']['subscriberCount']
+
+    @classmethod
+    def get_service(cls):
+        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=cls.channel_id
+                                               ).execute()
+        return video_response
+
+    def to_json(self, file_name):
+        data = {"channel_id": self.channel_id, "title": self.title,
+                "url": self.url, "description": self.description,
+                "view_count": self.view_count, "subscriber_count": self.subscriber_count,
+                "video_count": self.video_count}
+
+        with open(file_name, "w") as json_file:
+            json.dump(data, json_file)
+
+
+
